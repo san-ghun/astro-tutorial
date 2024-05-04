9ff11e26dfd3e104dac1bd5b49850aa1ae337054
Sanghun <po4865@gmail.com>
2024-05-04 16:04:51 +0200

    feat: Add Preact to the project - Add a Preact written greeting banner for the home page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dktmxmfh-tutorial.netlify.app",
+  integrations: [preact()]
 });
